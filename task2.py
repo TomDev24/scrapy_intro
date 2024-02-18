82f08972443a8a61340e84abf7ee2ca18997a372
TomDev24 <mail.com67747@gmail.com>
2024-02-18 18:33:23 +0300

    task2 solved. Added simple README

---
+++ task2.py
@@ -0,0 +1,25 @@
+import scrapy
+from pathlib import Path
+
+keys = ['team_name', 'year', 'wins', 'losses', 'ot-losses', 'win-pct', 'goals-for', 'goals-against', 'diff']
+
+class FormSpider(scrapy.Spider):
+    name="form_spider"
+    start_urls = [
+        "https://www.scrapethissite.com/pages/forms/"
+    ]
+
+    def parse(self, response):
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={"q": "New York"},
+            callback=self.form_filled
+        )
+
+    def form_filled(self, response):
+        rows = response.css("tr.team")
+        for row in rows:
+            values = row.css('td::text').getall()
+            values = [x.strip() for x in values]
+            yield dict(zip(keys, values))
+
